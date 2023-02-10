ee18460727d23c0479e0178540ef38e7bb14397c
anjor <anjor@umd.edu>
2023-02-10 15:58:30 +0000

    commp

---
+++ utils/deal_param_utils.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ./splitter.sh
+
+fetch_commp() {
+  OUTPUT=$1
+  echo $OUTPUT | grep -o "CommPCid.*" | tr -s ' ' | cut -d: -f2 | cut -d ' ' -f2
+}
+
+export -f fetch_commp
+
+fetch_padded_piece_size() {
+  OUTPUT=$1
+  echo $OUTPUT | grep -o "Padded piece.*" | tr -s ' ' | cut -d: -f 2 | cut -d ' ' -f2
+}
+
+export -f fetch_padded_piece_size
+
+run_stream_commp() {
+  FILE=$1
+  output=$(cat $FILE | stream-commp 2>&1)
+
+  commp=$(fetch_commp "$output")
+  padded_piece_size=$(fetch_padded_piece_size "$output")
+  parent_file=$(get_parent_file $FILE)
+
+  echo $(date), $parent_file, $FILE, $commp, $padded_piece_size 
+}
+
+export -f run_stream_commp
+
