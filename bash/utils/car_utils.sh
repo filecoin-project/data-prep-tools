6bbe62554594150de56ab7757179679d71807a1c
anjor <anjor@umd.edu>
2023-02-16 11:40:43 +0000

    utils

---
+++ bash/utils/car_utils.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Car generators
+ipld_go_car() {
+  FILE=$1
+  echo "Generating car file for $FILE using ipld/go-car"
+  car c  --version 1 -f ${FILE}.car $FILE
+  echo "Done generating car file for $FILE using ipld/go-car"
+}
+export -f ipld_go_car
+
+stream_dagger() {
+  FILE=$1
+  echo "Generating car file for $FILE using stream-dagger" 
+  cat $FILE | /Users/anjor/repos/ribasushi/DAGger/bin/stream-dagger --ipfs-add-compatible-command="--cid-version=1" --emit-stdout=car-v0-pinless-stream > ${FILE}.car
+  echo "Done generating car file for $FILE using stream-dagger"
+}
+export -f stream_dagger
+
+
+# Car splitters
+carbites() {
+  FILE=$1
+  SIZE=$2
+  echo "Splitting car file for $FILE using carbites"
+  cat $FILE | /Users/anjor/repos/alanshaw/go-carbites/cmd/carbites split --size $SIZE $FILE
+}
+export -f carbites
