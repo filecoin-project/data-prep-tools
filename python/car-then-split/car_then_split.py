150a046096177cf88fd07075da1509fd1e416963
anjor <anjor@umd.edu>
2023-03-10 10:39:30 +0000

    simplify

--- python/car-then-split/car_then_split.py
+++ python/car-then-split/car_then_split.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import csv
-import glob
 import os.path
 import shutil
 from datetime import datetime
@@ -9,34 +8,7 @@ from datetime import datetime
 from dataprep_tools.car_generators import IpldGoCar
 from dataprep_tools.car_splitters import Carbites
 from dataprep_tools.commp_calculators import StreamCommP
-
-
-class CarThenSplit:
-
-    def __init__(self, data, chunk_size, car_generator,
-                 car_splitter, commp_calculator):
-        self.data_dir = data
-        self.chunk_size = chunk_size
-        self.car_generator = car_generator
-        self.car_splitter = car_splitter
-        self.commp_calculator = commp_calculator
-
-    def find_car_files(self):
-        for file in glob.glob(self.data_dir + "**/*.car", recursive=True):
-            if os.path.isfile(file):
-                yield file
-
-    def generate_car(self, file, output=None):
-        if not output:
-            output = file + ".car"
-        self.car_generator.generate_car(file, output)
-
-    def split_car(self, file):
-        self.car_splitter.split_car(file, self.chunk_size)
-
-    def calculate_deal_params(self, file):
-        return self.commp_calculator.calculate_commp(file)
-
+from dataprep_tools.utils.car_then_split import CarThenSplit
 
 if __name__ == '__main__':
     data_dir = "/Users/anjor/repos/anjor/filecoin_data_prep_tools/data"
@@ -44,7 +16,7 @@ if __name__ == '__main__':
     car_then_split = CarThenSplit(
         data=data_dir,
         chunk_size=1000000000,
-        car_generator=IpldGoCar(), # IpldGoCar's generate_car function can take in a glob as data_dir
+        car_generator=IpldGoCar(),  # IpldGoCar's generate_car function can take in a glob as data_dir
         car_splitter=Carbites("/Users/anjor/repos/alanshaw/go-carbites/cmd/carbites"),
         commp_calculator=StreamCommP()
     )
