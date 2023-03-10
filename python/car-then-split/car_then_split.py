4e4519cf9850b071b4f1302c87896ba44c1a99b0
anjor <anjor@umd.edu>
2023-03-10 13:53:51 +0000

    root cid

--- python/car-then-split/car_then_split.py
+++ python/car-then-split/car_then_split.py
@@ -4,6 +4,7 @@ import csv
 import os.path
 import shutil
 from datetime import datetime
+from subprocess import check_output
 
 from dataprep_tools.car_generators import IpldGoCar
 from dataprep_tools.car_splitters import Carbites
@@ -13,10 +14,11 @@ from dataprep_tools.utils.car_then_split import CarThenSplit
 if __name__ == '__main__':
     data_dir = "/Users/anjor/repos/anjor/filecoin_data_prep_tools/data"
     metadata_file = os.path.join(data_dir, "__metadata.csv")
+    go_car = IpldGoCar()
     car_then_split = CarThenSplit(
         data=data_dir,
         chunk_size=1000000000,
-        car_generator=IpldGoCar(),  # IpldGoCar's generate_car function can take in a glob as data_dir
+        car_generator=go_car,  # IpldGoCar's generate_car function can take in a glob as data_dir
         car_splitter=Carbites("/Users/anjor/repos/alanshaw/go-carbites/cmd/carbites"),
         commp_calculator=StreamCommP()
     )
@@ -28,15 +30,16 @@ if __name__ == '__main__':
     os.chdir(data_dir)
 
     car_then_split.split_car(car_file)
+    root_cid = go_car.get_root_cid(car_file)
     os.unlink(car_file)
 
     f = open(metadata_file, 'w')
     writer = csv.writer(f)
-    writer.writerow(["timestamp", "original data", "car file", "piece cid", "padded piece size"])
+    writer.writerow(["timestamp", "original data", "car file", "root_cid", "piece cid", "padded piece size"])
     car_files = car_then_split.find_car_files()
     for car_file in car_files:
         deal_params = car_then_split.calculate_deal_params(car_file)
-        row = [datetime.now(), data_dir, car_file, deal_params['commp'], deal_params['padded_piece']]
+        row = [datetime.now(), data_dir, car_file, root_cid, deal_params['commp'], deal_params['padded_piece']]
 
         writer.writerow(row)
 
