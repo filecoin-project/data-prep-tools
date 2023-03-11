1bda0c931b564e35d766d4bd2960f6e3653d6cdb
anjor <anjor@umd.edu>
2023-03-11 23:20:00 +0000

    add ipfs-car-streaming

--- performance/scripts/test_tool.sh
+++ performance/scripts/test_tool.sh
@@ -16,10 +16,16 @@ ipfs-car() {
   time bash -c "ipfs-car --pack $FILE --output $FILE.car && cat $FILE.car | stream-commp" 2>&1 | grep real
 }
 
+ipfs-car-streaming() {
+  FILE=$1
+  echo -ne "ipfs-car2, ${FILE}, $(ls -lh $FILE | awk '{print $5}'), "
+  time bash -c "ipfs-car2 pack $FILE -o $FILE.car && cat $FILE.car | stream-commp" 2>&1 | grep real
+}
+
 streaming-wip() {
   FILE=$1
   echo -ne "streaming-wip, ${FILE}, $(ls -lh $FILE | awk '{print $5}'), "
-  time bash -c "cat $FILE | ~/repos/ribasushi/DAGger/bin/stream-dagger --ipfs-add-compatible-command="--cid-version=1"  --emit-stdout=car-v0-pinless-stream | tee $FILE.car | stream-commp" 2>&1 | grep real
+  time bash -c "cat $FILE | ~/repos/anjor/anelace/cmd/anelace/anelace --ipfs-add-compatible-command="--cid-version=1"  --emit-stdout=car-v1-stream | tee $FILE.car | stream-commp" 2>&1 | grep real
 }
 
 generate-car() {
@@ -43,6 +49,10 @@ case $TOOL in
 
   ipfs-car)
     ipfs-car $FILE
+    ;;
+
+  ipfs-car-streaming)
+    ipfs-car-streaming $FILE
     ;;
 
   streaming-wip)
