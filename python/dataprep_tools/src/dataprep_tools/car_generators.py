7c299499840c6d3622f704edd926c8fbc82e766d
anjor <anjor@umd.edu>
2023-03-13 12:09:51 +0000

    add streaming

--- python/dataprep_tools/src/dataprep_tools/car_generators.py
+++ python/dataprep_tools/src/dataprep_tools/car_generators.py
@@ -1,6 +1,8 @@
+import subprocess
 from abc import ABC, abstractmethod
 from os.path import dirname
 from subprocess import check_call, check_output
+import json
 
 
 class CarGenerator(ABC):
@@ -35,3 +37,37 @@ class IpldGoCar(CarGenerator):
 
     def get_root_cid(self, car_file):
         return check_output([self.generator, 'root', car_file]).decode()
+
+class Anelace(CarGenerator):
+    def __init__(self, generator='anelace'):
+        self.generator = generator
+        self.root = None
+
+    def generate_car(self, source_file, output_file):
+        generate_cmd=[
+            self.generator,
+            '--chunker=fixed-size_1048576',
+            '--collector=trickle_max-direct-leaves=4096_max-sibling-subgroups=8',
+            '--inline-max-size=36',
+            '--hash=sha2-256',
+            '--node-encoder=unixfsv1_merkledag-compat-protobuf',
+            '--emit-stdout=car-v1-stream',
+            '--emit-stderr=roots-jsonl'
+        ]
+        with open(source_file, 'r') as inp:
+            with open(output_file, 'w') as car:
+                pipe = subprocess.Popen(generate_cmd, stdin=inp, stdout=car, stderr=subprocess.PIPE)
+
+        _, std_err = pipe.communicate()
+        self.root = json.loads(std_err)['cid']
+
+    def get_root_cid(self, car_file):
+        return self.root
+
+
+if __name__ == '__main__':
+    anelace = Anelace('/Users/anjor/repos/anjor/anelace/cmd/anelace/anelace')
+
+    anelace.generate_car('file.txt', 'file.txt.car')
+
+    print(anelace.get_root_cid('file.txt.car'))
