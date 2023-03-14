1e5f02de8de190f9a04bbe7de2f29698931cbbbb
anjor <anjor@umd.edu>
2023-03-14 14:25:48 +0000

    remove dist

--- bash/dataprep_tools/car_splitters.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-carbites() {
-  FILE=$1
-  SIZE=$2
-  echo "Splitting car file for $FILE using carbites"
-  /Users/anjor/repos/alanshaw/go-carbites/cmd/carbites split --size $SIZE $FILE
-}
-export -f carbites
